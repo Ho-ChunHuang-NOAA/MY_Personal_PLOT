b748608aed67b7424af82900ecec8917bd01e960
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2023-06-29 14:04:56 +0000

    re-create scripts for v7 exp using c##.yyyymmdd

--- cmaq.python/v7exp_plot_airnow_cmaq.sh
+++ cmaq.python/v7exp_plot_airnow_cmaq.sh
@@ -30,32 +30,27 @@ else
 fi
 pm=`echo ${prodmachine} | cut -c1-1`
 if [ -s prod_info_list ]; then /bin/rm -f prod_info_list; fi
-MSG="$0 beg_date end_date"
+MSG="$0 envir beg_date end_date"
 if [ "${hl}" != "${pm}" ]; then
-    if [ $# -lt 1 ]; then
+    if [ $# -lt 3 ]; then
         echo ${MSG}
         exit
     fi
-    if [ $# -eq 1 ]; then
-        FIRSTDAY=$1
-        LASTDAY=$1
-    else
-        FIRSTDAY=$1
+    if [ $# -eq 2 ]; then
+        envir=$1
+        FIRSTDAY=$2
         LASTDAY=$2
+    else
+        envir=$1
+        FIRSTDAY=$2
+        LASTDAY=$3
     fi
-    cdate=${TODAY}"00"
-    TDYm1=$(${NDATE} -24 ${cdate} | cut -c1-8)
-    TDYm2=$(${NDATE} -48 ${cdate} | cut -c1-8)
-    TDYm3=$(${NDATE} -72 ${cdate} | cut -c1-8)
-    TDYm4=$(${NDATE} -96 ${cdate} | cut -c1-8)
-    TDYm5=$(${NDATE} -120 ${cdate} | cut -c1-8)
 
     script_dir=/lfs/h2/emc/vpppg/save/${USER}/plot/cmaq.python
-    ## declare -a cyc=( 06 12 )
     NOW=${FIRSTDAY}
     while [ ${NOW} -le ${LASTDAY} ]; do
-        script_name=mplot_rrfs_overlay_qsub.py
-        declare -a exp=( v70c82 )
+        script_name=mplot_rrfs_exp_overlay_qsub.py
+        declare -a exp=( ${envir} )
         cd ${script_dir}
 	if [ -s ${script_name} ]; then
             for i in "${exp[@]}"; do
