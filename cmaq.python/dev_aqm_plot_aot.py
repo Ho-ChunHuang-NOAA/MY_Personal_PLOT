1901ae77027c81bcd7f979e1d30d2d220cb52100
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2023-07-18 14:24:41 +0000

    modify aot code

--- cmaq.python/dev_aqm_plot_aot.py
+++ cmaq.python/dev_aqm_plot_aot.py
@@ -48,14 +48,10 @@ for line in rfile:
         if ver_name[1] == "aqm_ver":
             aqm_ver=ver[1]
 rfile.close()
+
 if aqm_ver=="":
     aqm_ver="v6.1"
 print("aqm_ver="+aqm_ver)
-wgrib2=os.environ['WGRIB2']
-if wgrib2 == "":
-    print("No definition of WGRIB2 can be found, please load module wgrib2/2.0.8")
-    sys.exit()
-
 stmp_dir="/lfs/h2/emc/stmp/"+user
 if not os.path.exists(stmp_dir):
     os.mkdir(stmp_dir)
@@ -115,7 +111,7 @@ H_date_format = "%H"
 date_inc = datetime.timedelta(hours=24)
 hour_inc = datetime.timedelta(hours=1)
 
-var=[ "aot" ]
+var=[ "aod" ]
 num_var=len(var)
 print("var length = "+str(num_var))
 
@@ -157,9 +153,10 @@ grdcro2d_date=msg.strftime("%Y%m%d")
 ## sys.exit()
 
 comout="/lfs/h1/ops/prod/com/aqm/"+aqm_ver
+comout="/lfs/h2/emc/ptmp/jianping.huang/emc.para/com/aqm/v7.0"
 usrout="/lfs/h2/emc/physics/noscrub/"+os.environ['USER']+"/verification/aqm/"+envir
-usrout="/lfs/h2/emc/vpppg/noscrub/"+os.environ['USER']+"/verification/aqm/"+envir
 usrout="/lfs/h2/emc/physics/noscrub/"+os.environ['USER']+"/com/aqm/"+envir
+usrout="/lfs/h2/emc/vpppg/noscrub/"+os.environ['USER']+"/verification/aqm/"+envir
 
 if not os.path.exists(comout):
     if not os.path.exists(usrout):
@@ -192,7 +189,7 @@ else:
     rlat1 = [   45., 40., 70.0,   51.0,    50.0,   54.5,   48.0,   52.0,   38.0,   45.0,   52.0,   40.0,   41.8,   72.0,   23.0,   70.0 ]
 xsize = [     10, 10, 10,     10,       8,      8,      8,      8,      8,      8,      8,      8,     10,      8,      8,     10 ]
 ysize = [      5, 5, 8,      8,       8,      8,      8,      8,      8,      8,      8,      8,      5,      8,      8,     8 ]
-if 1 == 1:
+if 1 == 2:
     iplot = [    0, 0,   1,      1,       1,      1,      1,      1,      1,      1,      1,      1,      1,      0,      0, 1 ]
 else:
     iplot = [    0,  0, 0,      1,       0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0, 0 ]
@@ -208,14 +205,11 @@ while date <= edate:
         date = date + date_inc
         continue
     
-    flag_ak = "no"
-    flag_hi = "no"
-
     for cyc in cycle:
         cycle_time="t"+cyc+"z"
         msg=datetime.datetime.now()
         print("Start processing "+date.strftime(YMD_date_format)+" "+cyc+" Current system time is :: "+msg.strftime("%Y-%m-%d %H:%M:%S"))
-        s1_title="CMAQ "+fig_exp.upper()+" "+date.strftime(YMD_date_format)+" "+cycle_time
+        s1_title="Online CMAQ "+fig_exp.upper()+" "+date.strftime(YMD_date_format)+" "+cycle_time
         fcst_ini=datetime.datetime(date.year, date.month, date.day, int(cyc[0:2]))
 
         ## metfilein=metout+"/cs."+grdcro2d_date+"/aqm."+cyc+".grdcro2d.ncf"
@@ -230,18 +224,38 @@ while date <= edate:
 
         for ivar in range(0,num_var):
             fcst_hour=fcst_ini
-            figdir = figout+"/aqm"+"_"+envir+"_"+date.strftime(YMD_date_format)+"_"+var[ivar]+cycle_time
+            ## figdir = figout+"/aqm"+"_"+envir+"_"+date.strftime(YMD_date_format)+"_"+var[ivar]+cycle_time
+            figdir = figout+"/c82"+"_"+envir+"_"+date.strftime(YMD_date_format)+"_"+var[ivar]+cycle_time
             print(figdir)
             if os.path.exists(figdir):
                 shutil.rmtree(figdir)
             os.makedirs(figdir)
             print("working on "+date.strftime(YMD_date_format)+" "+cycle_time+" "+var[ivar])
             flag_read_latlon="no"
-            hour_end = 73
+            hour_end = 2 
             for fcst_hr in range(1,hour_end):
                 str_fcst_hr=str(fcst_hr)
                 fhh=str_fcst_hr.zfill(2)
+                fhh3=str_fcst_hr.zfill(3)
+                if var[ivar] == "aod":
+                    file_hdr="aqm."+cycle_time+"."+var[ivar]+".f"+fhh3
+                    aqmfilein=comout+"/c82."+date.strftime(YMD_date_format)+"/"+cyc+"/"+file_hdr+".nc"
+                    if os.path.exists(aqmfilein):
+                        print(aqmfilein+" exists")
+                        cs_aqm = netcdf.Dataset(aqmfilein)
+                        cs_lat = cs_aqm.variables['lat'][:,:]
+                        cs_lon = cs_aqm.variables['lon'][:,:]
+                        aot_cs = cs_aqm.variables['aod'][0,:,:]
+                        cs_aqm.close()
+                    else:
+                        print("Can not find "+aqmfilein)
+
                 if var[ivar] == "aot":
+                    wgrib2=os.environ['WGRIB2']
+                    if wgrib2 == "":
+                        print("No definition of WGRIB2 can be found, please load module wgrib2/2.0.8")
+                        sys.exit()
+
                     file_hdr="aqm."+cycle_time+"."+var[ivar]+".f"+fhh+".148"
                     aqmfilein=comout+"/cs."+date.strftime(YMD_date_format)+"/"+file_hdr+".grib2"
                     outfile=working_dir+"/"+file_hdr+"."+date.strftime(YMD_date_format)+"."+cycle_time+".nc"
@@ -252,122 +266,80 @@ while date <= edate:
                         cs_aqm = netcdf.Dataset(aqmfilein)
                         cs_lat = cs_aqm.variables['latitude'][:,:]
                         cs_lon = cs_aqm.variables['longitude'][:,:]
-                        latmax=np.amax(cs_lat)
-                        latmin=np.amin(cs_lat)
-                        lonmax=np.amax(cs_lon)
-                        lonmin=np.amin(cs_lon)
-                        ## print("from "+str(latmin)+" to "+str(latmax))
-                        ## print("from "+str(lonmin)+" to "+str(lonmax))
                         aot_cs = cs_aqm.variables['AOTK_1sigmalevel'][0,:,:]
                         cs_aqm.close()
                     else:
                         print("Can not find "+aqmfilein)
 
-            ## if flag_ak == "no" and iplot[num_reg-3] == 1:
-            ##     iplot[num_reg-3] = 0
-            ## if flag_hi == "no" and iplot[num_reg-2] == 1:
-            ##     iplot[num_reg-2] = 0
-            ## print("iplot length = "+str(num_reg))
-                    fcst_hour=fcst_hour+hour_inc
-                    s2_title = fcst_hour.strftime(YMDH_date_format)+"00V"+fhh
-                    msg=datetime.datetime.now()
-                    s3_title="Total AOD"
-                    clevs = [ 0., 0.2, 0.4, 0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0 ]
-                    var_cs=aot_cs
-                    if flag_ak == "yes":
-                        var_ak=aot_ak
-                    if flag_hi == "yes":
-                        var_hi=aot_hi
-                    cmap = mpl.colors.ListedColormap([
-                           (0.8627,0.8627,1.0000), (0.6471,0.6471,1.0000),
-                           (0.0000,0.7843,0.7843), (0.0000,0.7843,0.0000),
-                           (0.6275,0.9020,0.1961), (0.9020,0.8627,0.1961), (0.9061,0.6863,0.1765),
-                           (0.9412,0.5098,0.1569), (0.9804,0.2353,0.2353), (1.0000,0.4118,0.7059), (0.1569,0.1569,0.1569),
-                           (0.3137,0.3137,0.3137), (0.4706,0.4706,0.4706)
-                           ])
-                    cmap.set_under((0.9412,0.9412,0.9412))
-                    cmap.set_over((0.6275,0.6275,0.6275))
-                    norm = mpl.colors.BoundaryNorm(boundaries=clevs, ncolors=cmap.N)
-                    gs = gridspec.GridSpec(1,1)
+                fcst_hour=fcst_hour+hour_inc
+                s2_title = fcst_hour.strftime(YMDH_date_format)+"00V"+fhh
+                msg=datetime.datetime.now()
+                s3_title="Total AOD"
+                clevs = [ 0., 0.2, 0.4, 0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0 ]
+                cmap = mpl.colors.ListedColormap([
+                       (0.8627,0.8627,1.0000), (0.6471,0.6471,1.0000),
+                       (0.0000,0.7843,0.7843), (0.0000,0.7843,0.0000),
+                       (0.6275,0.9020,0.1961), (0.9020,0.8627,0.1961), (0.9061,0.6863,0.1765),
+                       (0.9412,0.5098,0.1569), (0.9804,0.2353,0.2353), (1.0000,0.4118,0.7059), (0.1569,0.1569,0.1569),
+                       (0.3137,0.3137,0.3137), (0.4706,0.4706,0.4706)
+                       ])
+                cmap.set_under((0.9412,0.9412,0.9412))
+                cmap.set_over((0.6275,0.6275,0.6275))
+                norm = mpl.colors.BoundaryNorm(boundaries=clevs, ncolors=cmap.N)
+                gs = gridspec.GridSpec(1,1)
 
-                    title=s1_title+"\n"+s2_title+" "+s3_title
-                    pvar_cs = var_cs[:,:]
-                    if flag_ak == "yes":
-                        pvar_ak = var_ak[:,:]
-                    if flag_hi == "yes":
-                        pvar_hi = var_hi[:,:]
-                    for ireg in range(0,num_reg):
-                        if iplot[ireg] == 1:
-                            figarea=regname[ireg]
-                            extent=[ rlon0[ireg], rlon1[ireg], rlat0[ireg], rlat1[ireg] ]
-                            clat=0.5*(rlat0[ireg] + rlat1[ireg])
-                            clon=0.5*(rlon0[ireg] + rlon1[ireg])
-                            if figarea == "ak":
-                                aqmproj=ccrs.LambertConformal(central_longitude=clon, central_latitude=clat, standard_parallels=(57, 63), globe=None)
-                            elif figarea == "hi":
-                                aqmproj=ccrs.LambertConformal(central_longitude=clon, central_latitude=clat, standard_parallels=(19, 21), globe=None)
-                            else:
-                                aqmproj=ccrs.LambertConformal(central_longitude=clon, central_latitude=clat, false_easting=-58.775, false_northing=48.772, standard_parallels=(33, 45), globe=None)
-                            fig, ax = plt.subplots(figsize=(xsize[ireg],ysize[ireg]))
-    
-                            ax = plt.axes(projection=aqmproj)
-                            ax.set_extent(extent)
-                            ax.coastlines('50m')
-                            states_provinces = cfeature.NaturalEarthFeature(
-                                 category='cultural',
-                                 name='admin_1_states_provinces_lines',
-                                 scale='50m',
-                                 facecolor='none')
-                            ax.add_feature(states_provinces, facecolor='none', edgecolor='gray')
-                            ## rivers_50m = cfeature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', '50m')
-                            ## ax.add_feature(cfeature.LAND, edgecolor='black')
-                            ## ax.add_feature(cfeature.OCEAN, edgecolor='black')
-                            ## ax.add_feature(cfeature.COASTLINE)
-                            ax.add_feature(cfeature.BORDERS, facecolor='none', linestyle=':')
-                            ax.add_feature(cfeature.LAKES, facecolor='None', edgecolor='black', alpha=0.5)
-                            ## ax.add_feature(cfeature.RIVERS)
-                            if figarea == "ak":
-                                cf1 = ax.contourf(
-                                         ak_lon, ak_lat, pvar_ak,
-                                         levels=clevs, cmap=cmap, norm=norm, extend='both',
-                                         transform=ccrs.PlateCarree() )
-                            elif figarea == "hi":
-                                cf1 = ax.contourf(
-                                         hi_lon, hi_lat, pvar_hi,
-                                         levels=clevs, cmap=cmap, norm=norm, extend='both',
-                                         transform=ccrs.PlateCarree() )
-                            else:
-                                cf1 = ax.contourf(
-                                         cs_lon, cs_lat, pvar_cs,
-                                         levels=clevs, cmap=cmap, norm=norm, extend='both',
-                                         transform=ccrs.PlateCarree() )
-                                if figarea == "dset":
-                                    if flag_ak == "yes":
-                                        ax.contourf(
-                                             ak_lon, ak_lat, pvar_ak,
-                                             levels=clevs, cmap=cmap, norm=norm, extend='both',
-                                             transform=ccrs.PlateCarree() )
-                                    if flag_hi == "yes":
-                                        ax.contourf(
-                                             hi_lon, hi_lat, pvar_hi,
-                                             levels=clevs, cmap=cmap, norm=norm, extend='both',
-                                             transform=ccrs.PlateCarree() )
-                            ax.set_title(title)
-                            ## cb2.set_label('Discrete intervals, some other units')
-                            fig.colorbar(cf1,cmap=cmap,orientation='horizontal',pad=0.015,aspect=80,extend='both',ticks=clevs,norm=norm,shrink=1.0,format=cbar_num_format)
-                            savefig_name = figdir+"/aqm."+figarea+"."+fig_exp+"."+date.strftime(YMD_date_format)+"."+cycle_time+"."+fhh+".aod.k1.png"
-                            plt.savefig(savefig_name, bbox_inches='tight')
-                            plt.close()
+                title=s1_title+"\n"+s2_title+" "+s3_title
+                pvar_cs = aot_cs
+                for ireg in range(0,num_reg):
+                    if iplot[ireg] == 1:
+                        figarea=regname[ireg]
+                        extent=[ rlon0[ireg], rlon1[ireg], rlat0[ireg], rlat1[ireg] ]
+                        clat=0.5*(rlat0[ireg] + rlat1[ireg])
+                        clon=0.5*(rlon0[ireg] + rlon1[ireg])
+                        if figarea == "ak":
+                            aqmproj=ccrs.LambertConformal(central_longitude=clon, central_latitude=clat, standard_parallels=(57, 63), globe=None)
+                        elif figarea == "hi":
+                            aqmproj=ccrs.LambertConformal(central_longitude=clon, central_latitude=clat, standard_parallels=(19, 21), globe=None)
+                        else:
+                            aqmproj=ccrs.LambertConformal(central_longitude=clon, central_latitude=clat, false_easting=-58.775, false_northing=48.772, standard_parallels=(33, 45), globe=None)
+                        fig, ax = plt.subplots(figsize=(xsize[ireg],ysize[ireg]))
+
+                        ax = plt.axes(projection=aqmproj)
+                        ax.set_extent(extent)
+                        ax.coastlines('50m')
+                        states_provinces = cfeature.NaturalEarthFeature(
+                             category='cultural',
+                             name='admin_1_states_provinces_lines',
+                             scale='50m',
+                             facecolor='none')
+                        ax.add_feature(states_provinces, facecolor='none', edgecolor='gray')
+                        ## rivers_50m = cfeature.NaturalEarthFeature('physical', 'rivers_lake_centerlines', '50m')
+                        ## ax.add_feature(cfeature.LAND, edgecolor='black')
+                        ## ax.add_feature(cfeature.OCEAN, edgecolor='black')
+                        ## ax.add_feature(cfeature.COASTLINE)
+                        ax.add_feature(cfeature.BORDERS, facecolor='none', linestyle=':')
+                        ax.add_feature(cfeature.LAKES, facecolor='None', edgecolor='black', alpha=0.5)
+                        ## ax.add_feature(cfeature.RIVERS)
+                        cf1 = ax.contourf(
+                                 cs_lon, cs_lat, pvar_cs,
+                                 levels=clevs, cmap=cmap, norm=norm, extend='both',
+                                 transform=ccrs.PlateCarree() )
+                        ax.set_title(title)
+                        ## cb2.set_label('Discrete intervals, some other units')
+                        fig.colorbar(cf1,cmap=cmap,orientation='horizontal',pad=0.015,aspect=80,extend='both',ticks=clevs,norm=norm,shrink=1.0,format=cbar_num_format)
+                        savefig_name = figdir+"/aqm."+figarea+"."+fig_exp+"."+date.strftime(YMD_date_format)+"."+cycle_time+"."+fhh+".aod.k1.png"
+                        plt.savefig(savefig_name, bbox_inches='tight')
+                        plt.close()
             ##
             ## scp by cycle and variable
             ##
-        os.chdir(figdir)
-        parta=os.path.join("/usr", "bin", "scp")
-        if 1 == 1 :
-            partb=os.path.join("hchuang@rzdm:", "home", "www", "emc", "htdocs", "mmb", "hchuang", "web", "fig", date.strftime(Y_date_format), date.strftime(YMD_date_format), cycle_time)
-        else:
-            partb=os.path.join("hchuang@rzdm:", "home", "www", "emc", "htdocs", "mmb", "hchuang", "transfer")
-        ## subprocess.call(['scp -p * '+partb], shell=True)
+##         os.chdir(figdir)
+##         parta=os.path.join("/usr", "bin", "scp")
+##         if 1 == 2 :
+##             partb=os.path.join("hchuang@rzdm:", "home", "www", "emc", "htdocs", "mmb", "hchuang", "web", "fig", date.strftime(Y_date_format), date.strftime(YMD_date_format), cycle_time)
+##         else:
+##             partb=os.path.join("hchuang@rzdm:", "home", "www", "emc", "htdocs", "mmb", "hchuang", "ftp")
+##         subprocess.call(['scp -p * '+partb], shell=True)
         msg=datetime.datetime.now()
         print("End   processing "+var[ivar])
         print("FIG DIR = "+figdir)
