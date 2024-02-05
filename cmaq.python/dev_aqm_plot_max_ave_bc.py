7a02a5a054451f408d673272eecf4d37032d7a67
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-02-05 22:25:26 +0000

    separate working dir and msg filename 3

--- cmaq.python/dev_aqm_plot_max_ave_bc.py
+++ cmaq.python/dev_aqm_plot_max_ave_bc.py
@@ -83,7 +83,7 @@ if not os.path.exists(working_root):
 
 os.chdir(working_root)
 
-msg_file=working_root+"/msg_read_"+start_date"_"+sel_cyc
+msg_file=working_root+"/msg_read_"+start_date+"_"+sel_cyc
 cmd="cat /etc/cluster_name"
 subprocess.call([cmd+" > "+msg_file], shell=True)
 cmd="cat /etc/wcoss.conf | grep cluster_name | awk -F\":\" '{print $2}'"
