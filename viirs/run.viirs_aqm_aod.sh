a4af6f07c5568eef9ae88e6b99367b200ae99537
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-11-14 16:25:33 +0000

    incremental changes 11/14/2024 1

--- viirs/run.viirs_aqm_aod.sh
+++ viirs/run.viirs_aqm_aod.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+TODAY=`date +%Y%m%d`
+
 MSG="USAGE $0 obs_sat (default:viirs) model_grid [default:aqm|hysplit|ngac] YYYYMMDD_BEG YYYYMMDD_END"
    obs_name=viirs
    mdl_name=aqm
@@ -26,21 +28,21 @@ MSG="USAGE $0 obs_sat (default:viirs) model_grid [default:aqm|hysplit|ngac] YYYY
    module load prod_envir/${prod_envir_ver}
    source $HOMEevs/dev/modulefiles/aqm/aqm_prep.sh
 
-TODAY=`date +%Y%m%d`
-
 set -x
 
-TODAY=`date +%Y%m%d`
+OBSVDIR=/lfs/h2/emc/physics/noscrub/${USER}/VIIRS_AOD/AOD
 output_root=/lfs/h2/emc/physics/noscrub/${USER}/VIIRS_AOD/REGRID
 mkdir -p ${output_root}
-OBSVDIR=/lfs/h2/emc/physics/noscrub/${USER}/VIIRS_AOD/AOD
 hpss_root=/5year/NCEPDEV/emc-naqfc/${USER}
 log_dir=/lfs/h2/emc/ptmp/${USER}/batch_logs
 mkdir -p ${log_dir}
 
 ## daily fron ftp includes hpss aracived of regrid output
-flag_hpss_archive=yes
 flag_hpss_archive=no
+flag_hpss_archive=yes
+
+ftp_script_dir=/u/ho-chun.huang/trans_rsync_scp
+ftp_script_name=w2h_viirs_regrid_aod.py
 
    ## Store temporary map and G16 pixel netCDF file, e.g.,
    ## /lfs/h2/emc/ptmp/${USER}/METPLUS_TMP/CONUS_2500_1500_56_-56_-101360_12.2.2_to_Lambert Conformal.grid_map or
@@ -48,6 +50,8 @@ flag_hpss_archive=no
    working_dir=/lfs/h2/emc/stmp/${USER}/working/viirsaod2${mdl_name}
    mkdir -p ${working_dir}
    case ${mdl_name} in
+      rrfs) value=4
+           mod_file=/lfs/h2/emc/vpppg/save/${USER}/plot/parm/rrfs.aot.grid;;
       aqmv7) value=4
            mod_file=/lfs/h2/emc/vpppg/save/${USER}/plot/parm/aqm.aot.793.grid;;
       aqm) value=4
@@ -94,16 +98,8 @@ flag_hpss_archive=no
          HRNOW=$(${NDATE} +1 ${HRNOW})
       done
       if [ "${flag_hpss_archive}" == "yes" ]; then
-         cd ${output_root}
-         YY=`echo ${NOW} | cut -c1-4`
-         YM=`echo ${NOW} | cut -c1-6`
-         hpssdir=${hpss_root}/VIIRS_L3_AOD_REGRID/${YY}/${YM}
-         hsi mkdir  ${hpssdir}
-         if [ -d ${mdl_name}.${NOW} ]; then
-            htar -cf ${hpssdir}/${mdl_name}.${NOW}.tar ${mdl_name}.${NOW} > ${log_dir}/${mdl_name}.${NOW}.viirs.aod.regrid.tar.log 2>&1 &
-         else
-            echo "Can not find ${output_root}/${mdl_name}.${NOW}"
-         fi
+          cd ${ftp_script_dir}
+          python ${ftp_script_name} ${mdl_name} ${NOW} ${NOW}
       fi
       cdate=${NOW}"00"
       NOW=$(${NDATE} +24 ${cdate}| cut -c1-8)
