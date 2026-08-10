7d95a7d90638ba19da696176afdd2f14725b1508
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2026-08-10 17:55:52 +0000

    add aqmv8 plotting code

---
+++ cmaq.python/rzdm_aqmv8_plot_max_ave_overlay.py
@@ -0,0 +1,155 @@
+import os
+import numpy as np
+import netCDF4 as netcdf
+import re
+import maps2d_plot_util as maps2d_plot_util
+import warnings
+import logging
+import sys
+import datetime
+import shutil
+import subprocess
+
+user=os.environ['USER']
+
+script_dir=os.getcwd()
+print("Script directory is "+script_dir)
+
+### PASSED AGRUEMENTS
+if len(sys.argv) < 4:
+    print("you must set 4 arguments as model[prod|para|...] cycle[06|12|all]  start_date end_date")
+    sys.exit()
+else:
+    envir = sys.argv[1]
+    sel_cyc = sys.argv[2]
+    start_date = sys.argv[3]
+    end_date = sys.argv[4]
+
+stmp_dir="/lfs/h2/emc/stmp/"+user
+if not os.path.exists(stmp_dir):
+    os.mkdir(stmp_dir)
+
+ptmp_dir="/lfs/h2/emc/ptmp/"+user
+if not os.path.exists(ptmp_dir):
+    os.mkdir(ptmp_dir)
+
+log_dir=ptmp_dir+"/batch_logs"
+if not os.path.exists(log_dir):
+    os.mkdir(log_dir)
+
+py_code=sys.argv[0]
+nfind=py_code.find("py")
+if nfind == -1:
+    workid=py_code
+else:
+    workid=py_code[0:nfind-1]
+working_root=stmp_dir+"/"+envir+"_"+workid
+if not os.path.exists(working_root):
+    os.mkdir(working_root)
+
+os.chdir(working_root)
+
+msg_file=working_root+"/msg_"+start_date+"_"+sel_cyc
+cmd="cat /etc/cluster_name"
+subprocess.call([cmd+" > "+msg_file], shell=True)
+cmd="cat /etc/wcoss.conf | grep cluster_name | awk -F\":\" '{print $2}'"
+subprocess.call([cmd+" > "+msg_file], shell=True)
+if os.path.isfile(msg_file):
+    with open(msg_file, 'r') as sh:
+        line=sh.readline()
+        machine=line.rstrip()
+    sh.close()
+if machine.lower() == "dogwood":
+    remote="cactus"
+    remote_host="clogin01.wcoss2.ncep.noaa.gov"
+elif machine.lower() == "cactus":
+    remote="dogwood"
+    remote_host="dlogin01.wcoss2.ncep.noaa.gov"
+else:
+    print("System name not defined for this script")
+    sys.exit()
+
+cmd="cat /etc/wcoss.conf | grep sec_profile | awk -F\":\" '{print $2}'"
+subprocess.call([cmd+" > "+msg_file], shell=True)
+if os.path.isfile(msg_file):
+    with open(msg_file, 'r') as sh:
+        line=sh.readline()
+        machine_type=line.rstrip()
+        flag_primary=False
+        if machine_type.upper() == "PRIMARYSYS":
+            flag_primary=True
+    sh.close()
+
+msg="Current machine is "+machine
+if flag_primary:
+    msg=msg+" as PRIMARYSYS"
+else:
+    msg=msg+" as BACKUPSYS"
+print(msg)
+
+msg="Remote  machine is "+remote
+if not flag_primary:
+    msg=msg+" as PRIMARYSYS"
+else:
+    msg=msg+" as BACKUPSYS"
+print(msg)
+
+sdate = datetime.datetime.strptime(start_date, '%Y%m%d')
+edate = datetime.datetime.strptime(end_date, '%Y%m%d')
+
+YMDH_date_format = "%Y%m%d/%H"
+YMD_date_format = "%Y%m%d"
+YM_date_format = "%Y%m"
+Y_date_format = "%Y"
+M_date_format = "%m"
+Month_date_format = "%B"
+D_date_format = "%d"
+H_date_format = "%H"
+date_inc = datetime.timedelta(hours=24)
+hour_inc = datetime.timedelta(hours=1)
+
+nfind=envir.find("_bc")
+if nfind == -1:
+    print("not a bias_correction cases")
+    BC_append=""
+else:
+    print("A bias_correction cases")
+    BC_append="_bc"
+
+var=[ "ozmax8", "ozmax1", "pmave24", "pmmax1" ]
+num_var=len(var)
+print("var length = "+str(num_var))
+
+if sel_cyc == "all":
+   cycle=[ "t06z", "t12z" ]
+elif sel_cyc == "06":
+   cycle=[ "t06z" ]
+elif sel_cyc == "12":
+   cycle=[ "t12z" ]
+else:
+    print("seletced cycle "+sel_cyc+" can not be recongized.")
+    sys.exit()
+
+figout=stmp_dir
+
+date=sdate
+while date <= edate:
+    for cyc in cycle:
+        for ivar in range(0,num_var):
+            jobid="aqm"+"_"+envir.lower()+"_"+date.strftime(YMD_date_format)+"_"+var[ivar]+"obs_"+cyc
+            figdir = figout+"/"+jobid
+            ##
+            ## scp by cycle and variable
+            ##
+            if os.path.exists(figdir):
+                os.chdir(figdir)
+                parta=os.path.join("/usr", "bin", "scp")
+                if 1 == 1 :
+                    partb=os.path.join("hchuang@rzdm:", "home", "www", "emc", "htdocs", "regional", "restricted", "aqm", "web", "fig", date.strftime(Y_date_format), date.strftime(YMD_date_format), cyc)
+                else:
+                    partb=os.path.join("hchuang@rzdm:", "home", "www", "emc", "htdocs", "mmb", "hchuang", "transfer")
+                subprocess.call(['scp -p * '+partb], shell=True)
+                print("FIG DIR = "+figdir)
+        msg=datetime.datetime.now()
+        print("End   processing "+date.strftime(YMD_date_format)+" Current system time is :: "+msg.strftime("%Y-%m-%d %H:%M:%S"))
+    date = date + date_inc
